f19437f8f16a3758620a01c7629fd0ba272015fc
Данаил Захариев <85960984+DanailZahariev@users.noreply.github.com>
2021-08-26 21:17:18 +0300

    Initial commit

---
+++ Topics/Relational operators/Is between/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int first = numbers[0];
+        int second = numbers[1];
+        int third = numbers[2];
+        if (first < second && second < third ) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+}
